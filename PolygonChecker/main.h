b5ba803ffbe372f29cbf062dfa99e21430e06620
Jasmine-1007 <jasminelyvia07@gmail.com>
2024-11-14 21:52:26 -0500

    include change in declaration

--- PolygonChecker/main.h
+++ PolygonChecker/main.h
@@ -1,4 +1,5 @@
 #pragma once
 void printWelcome();
 int printShapeMenu();
+int RectFunction();
 int* getTriangleSides(int* triangleSides);
--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -1,4 +1,4 @@
 #pragma once
 char* analyzeTriangle(int side1, int side2, int side3);
-int calculateangle(int a, int b, int c);
-int printangle(int a, int b, int c);
+double calculateangle(int a, int b, int c);
+void printangle(int a, int b, int c);
