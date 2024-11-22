cb62d5eee88f9b71ad9dd456120c4a7f0340c384
Jasmine-1007 <jasminelyvia07@gmail.com>
2024-11-22 02:32:25 -0500

    create a test file and configure

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -8,7 +8,7 @@
 
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = "";
-	
+
 	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
 		result = "Not a triangle, the length cannot be negative";
 	}
@@ -16,13 +16,13 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 		result = "Cannot form a triangle, the sum of two sides must be bigger than the third for all three combinations.";
 	}
 	else if (side1 == side2 && side2 == side3) {
-		
+
 		result = "Equilateral triangle";
-			
-		}
-	else if ((side1 == side2 && side1 != side3) || 
+
+	}
+	else if ((side1 == side2 && side1 != side3) ||
 		(side1 == side3 && side1 != side2))
-	{	
+	{
 		double angle1 = calculateangle(side1, side2, side3);
 		result = "Isosceles triangle";
 	}
@@ -30,7 +30,7 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 		double angle2 = calculateangle(side1, side2, side3);
 		result = "Scalene triangle";
 	}
-	
+
 	return result;
 }
 
@@ -39,15 +39,15 @@ double calculateangle(int a, int b, int c) {
 
 	// Ensure the cosine value is within the valid range [-1, 1] for acos()
 	if (cosine < -1.0) { cosine = -1.0; }
-	if (cosine > 1.0){ cosine = 1.0;}
+	if (cosine > 1.0) { cosine = 1.0; }
 
 	double angle_radians = acos(cosine);
 
 	double angle = angle_radians * (180.0 / M_PI);
 	return angle;
 
-	}
-	
+}
+
 
 
 void printangle(int a, int b, int c) {
